eb4118bd250817760e01412048ca16b80df4dfab
Anand Muralidhar <anandmuralidhar111@gmail.com>
2018-12-05 20:49:58 -0800

    Integrated OKTA SSO

--- src/main/java/edu/phoenixRisers/RecipeBook/RecipeBookApplication.java
+++ src/main/java/edu/phoenixRisers/RecipeBook/RecipeBookApplication.java
@@ -1,16 +1,24 @@
 package edu.phoenixRisers.RecipeBook;
 
 import org.springframework.boot.SpringApplication;
-
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.security.Principal;
 
 @SpringBootApplication
+@EnableOAuth2Sso
+@RestController
 public class RecipeBookApplication {
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeBookApplication.class, args);
 	}
 	
+	@GetMapping("/index")
+    public String indexPage() {
+        return "index";
+    }
 }
